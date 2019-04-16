858d92de1b4674f9dde8301d8545cb388ac0ba54
Cian Morrin <cianm1994@gmail.com>
2019-04-16 16:09:51 +0100

    tidy code

--- java/src/ie/tudublin/UI.java
+++ java/src/ie/tudublin/UI.java
@@ -19,14 +19,7 @@ public class UI extends PApplet
     Button b;
     MovingRect mb;
     Radar r;
-    int xspacing = 16;   // How far apart should each horizontal location be spaced
-    int w;              // Width of entire wave
-
-    float theta = 0.0f;  // Start angle at 0
-    float amplitude = 75.0f;  // Height of wave
-    float period = 500.0f;  // How many pixels before the wave repeats
-    float dx;  // Value for incrementing X, a function of period and xspacing
-    float[] yvalues;  // Using an array to store height values for the wave
+    
 
 
     int selected1 = -1;
@@ -49,29 +42,11 @@ public class UI extends PApplet
         return keys[c] || keys [Character.toUpperCase(c)];
     }
 
-    void calcWave() {
-        // Increment theta (try different values for 'angular velocity' here
-        theta += 0.06;
+    
       
-        // For every x value, calculate a y value with sine function
-        float x = theta;
-        for (int i = 0; i < yvalues.length; i++) {
-          yvalues[i] = sin(x)*amplitude;
-          x+=dx;
-        }
-      }
-      
-      void renderWave() {
-        noStroke();
-        fill(255);
-        // A simple way to draw the wave with an ellipse at each location
-        for (int x = 0; x < yvalues.length; x++) {
-          ellipse(x*xspacing, (height-200)+yvalues[x], 16, 16);
-        }
-      }
 
     
-      void loadProducts()
+      void loadAliens()
       {
           Table table = loadTable("alien.csv", "header");
           for(TableRow tr:table.rows())
@@ -82,13 +57,13 @@ public class UI extends PApplet
       }
   
      
-      public void printProducts() {
+      public void printAliens() {
           for (Alien a : aliens) {
               System.out.println(a);
           }
       }
 
-      public void printBill() {
+      public void printAlienInfo() {
         for (Alien a1 : alieninfo) {
             System.out.println(a1);
         }
@@ -101,7 +76,7 @@ public class UI extends PApplet
     String alienPick = " ";
     
 
-      void drawProductButtons()
+      void drawAlienButtons()
      {
          stroke(204, 255, 255);
         line(0, 675, width, 675);
@@ -196,7 +171,7 @@ public class UI extends PApplet
     }
 
     
-    public void displayBill() {
+    public void displayAlienInfo() {
       fill(41, 45, 70);
       stroke(204, 255, 255);
       rect( (width / 2) - 200, 700, buttonWidth + 240, 180); // rectangle
@@ -213,36 +188,32 @@ public class UI extends PApplet
     public void settings()
     {
         fullScreen();
-        loadProducts();
-        printProducts();
-        printBill();
+        loadAliens();
+        printAliens();
+        printAlienInfo();
     }
 
     
 
     public void setup()
     {
-        w = 0+300;
-        dx = (TWO_PI / period) * xspacing;
-        yvalues = new float[w/xspacing];
+        
         s = new Compass(this, width / 2, height / 2, 5, 50);
         ch = new CentreHub(this, width, height, PI, QUARTER_PI, HALF_PI, TWO_PI, 0, 0); 
         sh = new Shapes(this, width / 2, height / 2);
         b = new Button(this, 50, 50, 100, 50, "I am a button");
         mb = new MovingRect(this, 200, 0, 150, 10, "I am bigger moving button");
         r = new Radar(this, 1, width - 200, 200, 100);
-       // noLoop();
+      
 
        loadData();
        printStars();
-      // planetborder = width * 0.1f;
+      
         
     }
 
     public void loadData() {
       Table table = loadTable("planets.csv", "header");
-
-    
 
       for (TableRow row : table.rows()) {
         Planet star = new Planet(row);
@@ -256,7 +227,6 @@ public class UI extends PApplet
     }
 }
 
-//float planetborder = 700;
 
 private void drawGrid() {
   textAlign(CENTER, CENTER);
@@ -306,8 +276,8 @@ public void drawStars() {
       r.update();
       r.render();
         
-      drawProductButtons();
-      displayBill();
+      drawAlienButtons();
+      displayAlienInfo();
 
 
       drawGrid();
